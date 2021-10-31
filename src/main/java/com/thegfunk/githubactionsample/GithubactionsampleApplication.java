5b9783ff96fe6ba783dfbe0729e17e61be9b8a2d
Guoneng Zhong <guoneng.list@gmail.com>
2021-10-31 20:56:27 +0100

    Initial workable spring with actuator

---
+++ src/main/java/com/thegfunk/githubactionsample/GithubactionsampleApplication.java
@@ -0,0 +1,13 @@
+package com.thegfunk.githubactionsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionsampleApplication.class, args);
+	}
+
+}
